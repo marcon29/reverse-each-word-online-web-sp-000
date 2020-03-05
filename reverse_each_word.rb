df2b0875810d70394de0fc4edc4cb8aef2386830
Mark Stabler <mark@stablerwritingservices.com>
2020-03-05 01:10:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@
 #   sentence.reverse.split(" ").reverse.join(" ")
 # end
 
-####### assignment dummy solution, uses .each)
+####### assignment dummy solution, uses .each
 # def reverse_each_word(sentence)
 #   array = sentence.split(" ")
 #   array_rev = []
@@ -11,7 +11,7 @@
 #   array_rev.join(" ")
 # end
 
-####### assignment tested solution, uses .collect)
+####### assignment tested solution, uses .collect
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   array.collect{|word| word.reverse}.join(" ")
